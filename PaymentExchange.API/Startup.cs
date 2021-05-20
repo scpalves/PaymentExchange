f623debcb2c4be75397f3faf3e5e2ce6a1061ca7
rui.alves <scpalves@gmail.com>
2021-05-20 12:38:50 +0100

    Create new tables in DB with migrations

--- PaymentExchange.API/Startup.cs
+++ PaymentExchange.API/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using PaymentExchange.Data.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +34,10 @@ namespace PaymentExchange.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PaymentExchange.API", Version = "v1" });
             });
+
+            services.AddDbContext<MyDbContext>(options =>
+            options.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +49,8 @@ namespace PaymentExchange.API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PaymentExchange.API v1"));
             }
+
+
 
             app.UseHttpsRedirection();
 
---
+++ PaymentExchange.Data/Migrations/20210520113624_Initial.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaymentExchange.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Invoices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ClientName = table.Column<string>(type: "varchar(100)", nullable: true),
+                    InvoiceTotalEarnings = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ClientPayment = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TotalMoneyDeduction = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TotalQuantityDeduction = table.Column<int>(type: "int", nullable: false),
+                    PayDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InvoiceLines",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    InvoiceLineId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ClientDeduction = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    QuantityDeduction = table.Column<int>(type: "int", nullable: false),
+                    InvoiceId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InvoiceLines", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InvoiceLines_Invoices_InvoiceId",
+                        column: x => x.InvoiceId,
+                        principalTable: "Invoices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InvoiceLines_InvoiceId",
+                table: "InvoiceLines",
+                column: "InvoiceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InvoiceLines");
+
+            migrationBuilder.DropTable(
+                name: "Invoices");
+        }
+    }
+}
