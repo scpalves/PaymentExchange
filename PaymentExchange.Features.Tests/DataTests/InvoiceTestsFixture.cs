8201b0f3976ecaba84cd8a81bd92eb442a2dc7fc
rui.alves <scpalves@gmail.com>
2021-05-24 18:27:44 +0100

    Made last tests

---
+++ PaymentExchange.Features.Tests/DataTests/InvoiceTestsFixture.cs
@@ -0,0 +1,72 @@
+﻿using PaymentExchange.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bogus;
+using Bogus.DataSets;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PaymentExchange.Features.Tests.DataTests
+{
+   
+
+    public class InvoiceTestsFixture 
+    {
+      
+
+     
+        public Invoice GenerateBusinessInvoice()
+        {
+
+            var invoice = new Invoice()
+            {
+                ClientName = "Teste",
+                InvoiceTotalEarnings = 25,
+                ClientPayment = 100,
+                InvoiceLines = new List<InvoiceLine>()
+            };
+
+  
+            return invoice;
+        }
+
+
+        public Invoice GenerateDataInvoice()
+        {
+
+            var invoice = new Invoice()
+            {
+                ClientName = "Teste",
+                InvoiceTotalEarnings = 25,
+                ClientPayment = 100,
+                TotalMoneyDeduction = 30,
+                TotalQuantityDeduction = 2,
+                InvoiceLines = new List<InvoiceLine>()
+            };
+
+            var line1 = new InvoiceLine()
+            {
+                ClientDeduction = 20,
+                QuantityDeduction = 1,
+            };
+
+            var line2 = new InvoiceLine()
+            {
+                ClientDeduction = 10,
+                QuantityDeduction = 1,
+            };
+
+       
+            invoice.InvoiceLines.Add(line1);
+            invoice.InvoiceLines.Add(line2);
+
+            return invoice;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ PaymentExchange.Features.Tests/InvoiceServiceTests.cs
@@ -0,0 +1,91 @@
+using Moq;
+using PaymentExchange.Business.Interfaces;
+using PaymentExchange.Features.Tests.DataTests;
+using System;
+using MediatR;
+using Xunit;
+using PaymentExchange.Business.Services;
+using System.Threading;
+using PaymentExchange.Data.Repository;
+using PaymentExchange.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+
+namespace PaymentExchange.Features.Tests
+{
+
+    public class InvoiceServiceTests
+    {
+      
+
+        InvoiceTestsFixture invoiceTestsFixture = new InvoiceTestsFixture();
+
+        [Fact(DisplayName = "InvoiceService_Add_ExecuteWithSucess")]
+        [Trait("Category", "Invoice Service Tests")]
+        public async void InvoiceService_Add_ExecuteWithSucess()
+        {
+ 
+            var invoice = invoiceTestsFixture.GenerateBusinessInvoice();
+            var invoiceRepo = new Mock<IinvoiceRepository>();
+            var notificator = new Mock<INotificator>();
+
+
+            var invoiceService = new InvoiceService(invoiceRepo.Object, notificator.Object);
+
+            await invoiceService.Create(invoice);
+
+            invoiceRepo.Verify(r => r.Create(invoice), Times.Once);
+        }
+
+        [Fact(DisplayName = "Get All Invoices")]
+        [Trait("Category", "Invoice Service Tests")]
+        public void InvoicesService_GetAllInvoices()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<MyDbContext>()
+           .UseInMemoryDatabase(databaseName: "Bill")
+            .Options;
+
+            using (var context = new MyDbContext(options))
+            {
+                var invoiceRepository = new InvoiceRepository(context);
+
+
+                var invoice = invoiceRepository.GetAllInvoice();
+
+            }
+
+            // Assert 
+            // Assert.True(invoice);
+            //Assert.False(clientes.Count(c => !c.Ativo) > 0);
+        }
+
+        [Fact(DisplayName = "Get Invoices By Id")]
+        [Trait("Category", "Invoice Service Tests")]
+        public void InvoicesService_GetInvoicesById()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<MyDbContext>()
+           .UseInMemoryDatabase(databaseName: "Bill")
+            .Options;
+
+            using (var context = new MyDbContext(options))
+            {
+                var invoiceRepository = new InvoiceRepository(context);
+
+
+                var invoice = invoiceRepository.GetInvoiceById(23);
+
+            }
+
+            // Assert 
+            // Assert.True(invoice);
+            //Assert.False(clientes.Count(c => !c.Ativo) > 0);
+        }
+    }
+
+}
+
+
+
+
